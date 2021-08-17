95ff86a2f2ee40329ece642a28e85e9c4558295d
Tolik Bilokrylov <enkorn1715@gmail.com>
2021-08-17 17:26:09 +0300

    fixed path

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 export function Navbar() {
@@ -23,49 +23,49 @@ export function Navbar() {
     <>
       <nav className={navbar ? 'navbar active' : 'navbar'} style={{ position: 'sticky' }}>
         <div className='navbar-container'>
-          <NavLink to={'/'} className='navbar-logo' onClick={closeMobileMenu}>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             Rick and Morty
             <i className="fas fa-globe-europe"/>
-          </NavLink>
+          </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
-              <NavLink
-                to={'/characters'}
+              <Link
+                to='/characters'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
                 Characters
-              </NavLink>
+              </Link>
             </li>
             <li className='nav-item'>
-              <NavLink
-                to={'/episodes'}
+              <Link
+                to='/episodes'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
                 Episodes
-              </NavLink>
+              </Link>
             </li>
             <li className='nav-item'>
-              <NavLink
-                to={'/locations'}
+              <Link
+                to='/locations'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
                 Locations
-              </NavLink>
+              </Link>
             </li>
             <li className='nav-item'>
-              <NavLink
-                to={'/myWatchList'}
+              <Link
+                to='/myWatchList'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
                 My watch list
-              </NavLink>
+              </Link>
             </li>
           </ul>
         </div>
